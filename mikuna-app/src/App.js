18e7cc074fb4495c7f4e109e98220fc42a5d3b1c
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-03-29 19:10:27 -0400

    creacion de layout

--- mikuna-app/src/App.js
+++ mikuna-app/src/App.js
@@ -1,22 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
 import ListaItems from './components/views/listaItems';
-import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
 import theme from './theme/theme';
-import { Container, Box } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
+import AppNavbar from './components/layout/appNavbar';
+import { BrowserRouter as Router ,Switch,Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider theme={theme}>
-        {/* <ListaItems /> */}
-        <Container>
-          <Box textAlign="center">
-            welcome a MIKUNA
-          </Box>
-        </Container>
-      </MuiThemeProvider>
-
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <AppNavbar></AppNavbar>
+          <Grid container>
+            {/* <Switch>
+              <Route path="/" exact component={ListaItems}></Route>
+            </Switch> */}
+          </Grid>
+        </MuiThemeProvider>
+      </Router>
     )
   }
 }
---
+++ mikuna-app/src/components/layout/appNavbar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import AppBar from '@material-ui/core/AppBar';
+import BarSession from './bar/barSession';
+
+class AppNavbar extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar position="static">
+                   <BarSession></BarSession>
+                </AppBar>
+                
+            </div>
+        )
+    }
+}
+
+export default AppNavbar;
