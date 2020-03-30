a95188472415d1c5f5f12b082ac4dc9a3820ee65
RICHARD GUTIERREZ CONDORI <rychard.gutierrez@gmail.com>
2020-03-29 23:29:56 -0400

    creacion de modulo registrarUsuario

---
+++ mikuna-app/src/components/seguridad/RegistrarUsuario.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { Container, Typography, Grid, TextField, Avatar, Button } from '@material-ui/core';
+import LockOutLineIcon from '@material-ui/icons/LockOutlined';
+
+const style = {
+    paper: {
+        margimTop: 8,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+    },
+    avatar: {
+        margin: 8,
+        backgroundColor: "#e53935"
+    },
+    form: {
+        width: "100%",
+        marginTop: 10
+    },
+    submit: {
+        marginTop: 15,
+        marginBottom: 20
+    }
+}
+
+export default class RegistrarUsuario extends Component {
+    state = {
+        usuario: {
+            nombre: "",
+            apellido: "",
+            email: "",
+            password: ""
+        }
+    }
+
+    onChange = e => {
+        let usuario = Object.assign({}, this.state.usuario);
+        usuario[e.target.name] = e.target.value;
+        let apellido = Object.assign({}, this.state.apellido);
+        apellido[e.target.name] = e.target.value;
+        let email = Object.assign({}, this.state.email);
+        email[e.target.name] = e.target.value;
+        let password = Object.assign({}, this.state.password);
+        password[e.target.name] = e.target.value;
+
+        this.setState({
+            usuario: usuario
+        })
+    }
+
+    registrarUsuario = e => {
+        e.preventDefault();
+        console.log("imprimir objeto", this.state.usuario);
+    }
+    render() {
+        return (
+            <Container maxWidth="md">
+                <div style={style.paper}>
+                    <Avatar style={style.avatar}>
+                        <LockOutLineIcon></LockOutLineIcon>
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Registra tu Cuenta
+                    </Typography>
+                    <form style={style.form}>
+                        <Grid container spacing={2}>
+                            <Grid item md={6} xs={12}>
+                                <TextField name="nombre" onChange={this.onChange} value={this.state.usuario.nombre} fullWidth label="Nombre"></TextField>
+                            </Grid>
+                            <Grid item md={6} xs={12}>
+                                <TextField name="apellido" onChange={this.onChange} value={this.state.usuario.apellido} fullWidth label="Apellido"></TextField>
+                            </Grid>
+                            <Grid item md={6} xs={12}>
+                                <TextField type="email" onChange={this.onChange} value={this.state.usuario.email} name="email" fullWidth label="Email"></TextField>
+                            </Grid>
+                            <Grid item md={6} xs={12}>
+                                <TextField type="password" onChange={this.onChange} value={this.state.usuario.password} name="password" fullWidth label="Password"></TextField>
+                            </Grid>
+                        </Grid>
+                        <Grid container justify="center">
+                            <Grid item xs={12} md={6}>
+                                <Button type="submit" onClick={this.registrarUsuario} variant="contained" fullWidth size="large" color="primary" style={style.submit}>
+                                    Registrar
+                                 </Button>
+                            </Grid>
+                        </Grid>
+                    </form>
+                </div>
+            </Container>
+        )
+    }
+}
